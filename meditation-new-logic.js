e9f6a31cdfb9064c5d63167d7e427aa4e9cfa3e6
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2019-06-26 19:29:36 -0500

    add new logic

--- meditation-new-logic.js
+++ meditation-new-logic.js
@@ -1,16 +1,35 @@
-
+let song = new Audio("calm-alarm.mp3");
+let hitIt = function() {
+  song.play();
+};
 
 let startTime;
-let duration = 120;
+let duration = 600;
+
+showStartTime = function() {
+  let startTime;
+  let min0 = duration / 60;
+  let sec0 = "00";
+
+
+    min0 = (min0 < 10) ? "0" + min0 : min0;
+
+  startTime = min0 + " : " + sec0;
+
+  document.getElementById("MyTimerDisplay").innerText = startTime;
+  document.getElementById("MyTimerDisplay").textContent = startTime;
+}
 
 function onMoreDuration() {
-  duration = duration + 120;
+  duration += 120;
+  showStartTime();
 }
 
 function onLessDuration() {
-  duration = duration - 120;
+    showStartTime();
+  duration -= 120;
   if (duration < 0) {
-    duration = 0;
+    duration = 120;
   }
 }
 
@@ -24,5 +43,22 @@ function every1Second() {
   let timeLeft = duration - secondsPassed;
   let minutes = Math.floor(timeLeft / 60);
   let seconds = timeLeft - (minutes * 60);
-  console.log(minutes + ':' + seconds);
+  let timer = minutes + ':' + seconds.toFixed(0)
+  console.log(timer);
+  document.getElementById("MyTimerDisplay").innerText = timer;
+  document.getElementById("MyTimerDisplay").textContent = timer;
+
+  let timerId = setTimeout(() => every1Second(), 1000);
+
+  if (minutes < 0) {
+    clearTimeout(timerId);
+    // this.sec = 60;
+
+    hitIt();
+  }
+}
+
+function run() {
+onStartPress();
+setTimeout(every1Second,1000);
 }
